5cc09361d532d75d79889ce80ee407e6ac60be2e
AdityaPanca <aditya.annecdo@gmail.com>
2019-09-23 16:38:28 +0700

    Dynamic Fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -1,8 +1,12 @@
 package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
+import polinema.ac.id.starterchapter05.activities.DynamicActivity;
+import polinema.ac.id.starterchapter05.activities.StaticActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handlerClickStaticFragment(View view) {
+        Intent intent =  new Intent(this, StaticActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlerClickDynamicFragment(View view) {
+        Intent intent =  new Intent(this, DynamicActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlerClickPraktikumFragment(View view) {
     }
 }
